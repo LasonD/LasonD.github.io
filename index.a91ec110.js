ab62f8011c346bdf3a66b10c65d6c7718ee96d2e
Oleksandr Barylo <sbarylo20023@gmail.com>
2021-11-02 21:49:30 +0200

    Updates

---
+++ index.a91ec110.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector("#like");e.addEventListener("click",(()=>e.style.color="red"!==e.style.color?"red":"black"))}(),document.querySelector("#current-date").innerHTML=new Date;
+//# sourceMappingURL=index.a91ec110.js.map
