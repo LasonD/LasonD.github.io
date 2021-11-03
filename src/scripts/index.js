e1ab6cc8a330c2d2b81fe98e3659f4ddc91cbe7a
Oleksandr Barylo <sbarylo20023@gmail.com>
2021-11-03 20:47:16 +0200

    Update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,26 @@
 (function () {
-    const $like = document.querySelector('#like');
-    $like.addEventListener('click', () => $like.style.color = $like.style.color !== 'red' ? 'red' : 'black');
+  const $like = document.querySelector("#like");
+  const $counter = document.querySelector("#like-counter");
+  $like.addEventListener("click", () => {
+    let isLike = $like.classList.contains("unliked");
+
+    let newIconColor, newBtnColor, counterContent;
+
+    if (isLike) {
+      $like.classList = ['liked'];
+      counterContent = "1";
+    } else {
+        $like.classList = ['unliked'];
+      counterContent = null;
+    }
+
+    $like.style.color = newIconColor;
+    $like.style.backgroundColor = newBtnColor;
+    $counter.textContent = counterContent;
+  });
 })();
 
 (function () {
-    const $date = document.querySelector('#current-date');
-    $date.innerHTML = new Date();
+  const $date = document.querySelector("#current-date");
+  setInterval(() => ($date.innerHTML = new Date().toLocaleString()), 1000);
 })();
